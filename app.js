4090a261c8b0684a3a6ec3a077b5f18fccfdf1a5
Thabass H <52821080+thabass@users.noreply.github.com>
2021-07-06 11:40:41 +0530

    Update app.js
    included body parser

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
+const bp = require('body-parser');
+
 const app=express();
 
 
+//let users=require('./routes/users');
+//app.use(bp.json());
+app.use(bp.urlencoded({extended:true}));
 
 app.use('/',require('./routes/index.js'));
 
-app.use('/users',require('./routes/users.js'));
+app.use('/screenshot',require('./routes/users.js'));
 
+
+
+
+//app.use('/yourimage',require('./routes/puppeteer.js'));
 
 
 
