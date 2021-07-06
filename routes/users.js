66f38f206aa62325d64230f195317d33d70e736a
Thabass H <52821080+thabass@users.noreply.github.com>
2021-07-06 11:50:15 +0530

    Update users.js
    puppeteer module included for taking screenshot

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const puppeteer=require('puppeteer');
+//const { v4: uuidv4 } = require('uuid');
 
-router.get('/yourimage',(req, res)=> res.render(index.html));
+router.post('/',function(req,res)
+{
+  let url=req.body.url;
+ 
+  console.log(url);
+
+const takescreenshot= async ()=>
+{
+//const url=document.getElementById('homepage').value;
+    const browser= await puppeteer.launch();
+    const page= await browser.newPage();
+
+    const options={ 
+        path: 'Screenshot/website.png',
+        fullpage: true,
+        omitbackgrid: true ,
+       };
 
 
-module.exports =router;
+    await page.goto(url);
+    await page.screenshot(options);
+
+    await browser.close();
+
+    takescreenshot();
+
+};
+
+
+})
+
+
+
+module.exports =router
