838f1eec3ca3897064eb7c392451dfec257a6159
Thabass H <52821080+thabass@users.noreply.github.com>
2021-07-08 01:40:46 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
+    
   });
 
 module.exports =router;
