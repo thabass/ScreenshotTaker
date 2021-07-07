d91257b36d60fd758c14f332a85aa907434caed4
Thabass H <52821080+thabass@users.noreply.github.com>
2021-07-08 01:39:52 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const puppeteer=require('puppeteer');
-//const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+// const { uuid } = require('uuid');
 
 router.post('/',function(req,res)
 {
-  let url=req.body.url;
- 
-  console.log(url);
+   const url = req.body.name;
+
+   console.log(url);
 
 const takescreenshot= async ()=>
 {
-//const url=document.getElementById('homepage').value;
+
     const browser= await puppeteer.launch();
     const page= await browser.newPage();
 
     const options={ 
         path: 'Screenshot/website.png',
         fullpage: true,
-        omitbackgrid: true ,
+        omitbackgrid: true,
+        captureBeyondViewport: true,
        };
 
 
     await page.goto(url);
     await page.screenshot(options);
+    //res.redirect(url);
+    res.sendFile(path.join(__dirname+'/contact.html'));
 
-    await browser.close();
-
-    takescreenshot();
+   await browser.close();
 
 };
-
-
-})
+takescreenshot();
+});
 
 
 
