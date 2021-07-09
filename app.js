29d2ffaea1a42e6687bee109a64104b60b2ddca0
Thabass H <52821080+thabass@users.noreply.github.com>
2021-07-10 00:00:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express');
 
-const bp = require('body-parser');
+const bodyParser= require('body-parser');
+
+//const fetch = require('fetch');
+
 
 const app=express();
 
 
 //let users=require('./routes/users');
-//app.use(bp.json());
-app.use(bp.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/',require('./routes/index.js'));
 
 app.use('/screenshot',require('./routes/users.js'));
 
-
-
-
-//app.use('/yourimage',require('./routes/puppeteer.js'));
-
+//app.use('/downloadimage',require('./routes/download.js'))
 
 
 
 
-const PORT=3000;
+const PORT=process.env.PORT||3000;
 
 app.listen(PORT,console.log(`successfully connected ${PORT}`));
